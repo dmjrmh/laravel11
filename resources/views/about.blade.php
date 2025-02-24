b9b053abcd6789ae5c9e4020cd7e1e32db425eec
Dame <gban1mj27@gmail.com>
2025-02-24 10:34:31 +0700

    added some blade components

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h3>Owner this page : {{ $name }}</h3>
-    <h1>About Page</h1>
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">About Me ?</h3>
+    <p class="text-lg">{{ $name }}</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,15 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
 
 Route::get('/blog/natus-vincere', function () {
@@ -22,12 +26,8 @@ Route::get('/blog/carl-johnson', function () {
     return view('carl-johnson');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
-
 Route::get('/about', function () {
-    return view('about', ['name' => 'Hehe']);
+    return view('about', ['title' => 'About', 'name' => 'Hehe']);
 });
 
 // 2 route baru 
