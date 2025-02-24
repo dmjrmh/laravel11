1366e8bcae90f620a3dc9546741fde10ca486816
Dame <gban1mj27@gmail.com>
2025-02-24 10:39:29 +0700

    test push

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">About Me ?</h3>
+    <h3 class="text-xl">About Me ??</h3>
     <p class="text-lg">{{ $name }}</p>
 </x-layout>
